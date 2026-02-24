6ab1916212ca8187ac2d7b78cf7834108257097c
HanuYat <jnjn1235@gmail.com>
2026-02-24 19:08:08 +0900

    포켓몬 클래스 생성
    클래스 내부에 포켓몬 이름과 공격력 등 정보를 필드화 해두었고, 생성자를 이용해 초기화 함.
    
    포켓몬의 정보를 볼 수 있는 메서드와 총 포켓몬 마리 수를 확인 할 수있는 메서드를 구축함.

---
+++ Pokemon.cs
@@ -0,0 +1,37 @@
+﻿class Pokemon
+{
+    private string _name;
+    private int _attack;
+    private int _defence;
+    private string _type;
+    private readonly int _maxHp;
+    private static int _pokemonCount;
+
+    public Pokemon(string name, int maxHp, int attack, int defence, string type)
+    {
+        _name = name;
+        _maxHp = maxHp;
+        _attack = attack;
+        _defence = defence;
+        _type = type;
+    }
+
+    public void ShowInfo()
+    {
+        if (_maxHp < 0)
+        {
+            Console.WriteLine("0보다 작은 숫자는 불가 합니다.");
+        }
+
+        else
+        {
+            Console.WriteLine($"[{_type}] {_name}: HP({_maxHp}), 공격력({_attack}), 방어력({_defence})");
+            _pokemonCount++;
+        }
+    }
+
+    public static void TotalCount()
+    {
+        Console.WriteLine($"총 포켓몬 마리 수: {_pokemonCount}");
+    }
+}
